3b45f9c436b8bcf869d13522b659bd40a04f59e7
demVadim <demmed85@mail.ru>
2021-11-19 16:05:15 +0800

    goto

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,16 @@
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* pWndow = glfwCreateWindow(640, 480, "BattleCity", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
